2898869750e9863265c525f1e9fae2ff18ac502e
Charles Duffy <charles.duffy@gmail.com>
2015-04-28 16:12:45 +1000

    initial rebar generated app files

---
+++ src/xndb-micro_app.erl
@@ -0,0 +1,16 @@
+-module(xndb-micro_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    xndb-micro_sup:start_link().
+
+stop(_State) ->
+    ok.
