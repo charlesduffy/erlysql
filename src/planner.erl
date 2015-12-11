752cd253070924c085643667ee54efc70cd5ece9
Charles Duffy <charles.duffy@gmail.com>
2015-12-11 18:38:59 +1100

    Initial version of planner. Can generate viable plans for SELECT queries. No plan optimisation is performed (except predicate push-down into scan nodes)

--- src/planner.erl
+++ src/planner.erl
@@ -55,145 +55,75 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
-
-%% @doc Make list of base relations from FromClause list
-%%
-%% This is a list of tuples of the form:
-%% { name:string , alias:string }
-%% 
-%% alias is blank if not present
-
-mk_brels([H|T]) -> mk_brels(T, { maps:get(name , H) , maps:get(alias, H, "") } ).
-
-mk_brels([H|T] , Acc) -> mk_brels(T , [ { maps:get(name , H) , maps:get(alias, H, "") } ] ++ Acc);
-
-mk_brels([], Acc) -> Acc.
-
-%% @doc Process a single FromClause entry and produce a scan node
-%% 
-%% To get scan node selection predicates, we need to search the Where
-%% clause to find relevant subexpressions (ie, those which do not compare
-%% attributes from different relations, becoming join conditions)
-%% 
-%% We search for the largest subexpressions containing only:
-%% 1. value expressions
-%% 2. Comparison operators
-%% 3. Attributes from the same relation
-
-mk_scnode(N) ->
-       #{   
-	    id => 0, 	
-	    module => seqscan , 
-	    target => 0,
-	    sourcelist => [], 
-    	    instruction => #{ 	
-				relname => maps:get(name, N), 
-				alias => maps:get(alias, N, ""),
-				projection => [] ,
-				selection =>  [] 
-			    }
-	}.
-			
-%% @doc Populate initial scan nodes with selection information
-get_sel(D)->
-	{ok}.
-
-get_sel(S , W) ->
-	{ok}	
-.
-
-%% @doc prototype subtree matcher
-%% 
-%% Find largest subtrees containing references to a single base
-%% relation 
-%% * Recurse left until leaf node, noting Node IDs (or pushing them on to a stack)
-%% * Note if colref, determine what relation it is member of
-%% * Go back up. 
-%% * Heuristic - check if immediate subtree matches OP,[col|val],[col|val] stereotype
-%%   -- if match, set Node ID entry for table in question to NodeID
-%%   -- No match, continue
-%% * Recurse right / left downward
-%%   -- 
-
-%% blah new idea
-%% recurse down creating a list for each node. 
-%% list contains all distinct relations in the subtree below
-%% push list recusrsively as progresses
-
-
-
-
-find_subtrees (ParseTree) ->
+find_subtrees1 (ParseTree) ->
 	RelMap = #{ "a" => "A" , "b" => "A" , "c" => "B" , "d" => "B" },
 
 %%	RelMap = #{ "c_custkey" => "customer" , "o_custkey" => "orders" , "l_orderkey" => "lineitem" , 
 %%		 "o_orderkey" => "orders" , "l_suppkey" => "lineitem" , "s_suppkey" => "supplier" , 
 %%		 "c_nationkey" => "customer" , "s_nationkey" => "supplier" , "n_nationkey" => "nation" , 
 %%		 "n_regionkey" => "nation" , "r_regionkey" => "region" , "r_name" => "region"},
-	find_subtrees( maps:get(where_clause, ParseTree),
-		       RelMap,
-		       1,		%% NodeID
-		       1  		%% NodeRank
-		       )		
+	find_subtrees1( maps:get(where_clause, ParseTree), RelMap)		
 .
-%% @doc process leaf nodes
-find_subtrees([ { type , colref } , { value , NodeVal } ], RelMap, NodeId, NodeRank) ->
-%%	io:fwrite("colref leaf ! my NodeID is: ~p~n", [ NodeId ]),	
-	[ { NodeId , NodeRank , [ NodeId ],  [ maps:get(NodeVal, RelMap) ] } ]
+
+find_subtrees1([ { type , colref } , { value , NodeVal } ], RelMap ) ->
+	[ {type , scan } , { predicate , [ { type , colref } , { value , NodeVal } ] }, { relation , maps:get(NodeVal, RelMap) }, { leaf , true } ]
 ;
 
-find_subtrees([ { type , _ } , { value , NodeVal } ], RelMap, NodeId, NodeRank) ->
-%%	io:fwrite("literal leaf ! my NodeID is: ~p~n", [ NodeId ]),	
-	[ { NodeId , NodeRank , [ NodeId ] , [] } ]
+find_subtrees1([ { type , NodeType } , { value , NodeVal } ], RelMap ) ->
+	[ {type , scan } , { predicate , [ { type , NodeType } , { value , NodeVal } ] }, { relation , null } , { leaf , true } ]
 ;
 
 
-%% @doc process interior nodes
-%% match an interior node , take node ID , relations map
-%% find_subtree returns a list of tuples, each tuple containing the nodeID
-%% of a previously traversed node and a list of relations below it
-%% We want to take the relation lists of our immediate child nodes
-%% and union them, to give the relation list of the current node
-%% The immediate child node lists will be the list heads of LRels and RRels.
-%% @todo rewrite this documentation
-find_subtrees ({ O , L , R }, RelMap, NodeId, NodeRank ) ->
+find_subtrees1 ({ Self , Lchild , Rchild }, RelMap ) ->
 	
-	LRels = find_subtrees(L, RelMap, NodeId + 1, NodeRank + 1), 	%% Process left child node
-%	io:fwrite("LRels is : ~p~n", [ LRels ]),	
-	[ LRelsH | _ ] = LRels,     		%% Get the head of the list returned by left child. This 
-					 		%% will be a tuple containing the NodeID and relation list
-					 		%% for the subtree rooted at the left child node.
+	LsubTree = find_subtrees1(Lchild, RelMap),
+	RsubTree = find_subtrees1(Rchild, RelMap), 	
 
-	{ _ , _ , LeftNodePath, LChRels } = LRelsH,			%% Match the left child node ID and relation list
+	% if same rel return scan
+	% if different rel return join op with trees
+	% if join + scan return join op
 
-	LNodeId = lists:max([ RelNid || { RelNid , _ , _ , _ } <- LRels ]),		%% Get the highet node ID from the subtree rooted at the left child node
-		
-	io:fwrite("LNodeId is : ~p~n", [ LNodeId ]),	
+	case
+		[ true || {type, scan} <- LsubTree , {type, scan} <- RsubTree  ] of
+	
+			[ true ] -> merge_subtrees1( 
+						[ Lrel || {relation, Lrel} <- LsubTree , {relation, Rrel} <- RsubTree , (Rrel==Lrel) or (Rrel == null) or (Lrel == null) ],
+						LsubTree,
+						RsubTree,
+						Self);
 
-	RRels = find_subtrees(R, RelMap, LNodeId + 1, NodeRank + 1), 	%% Process right child node
-
-
-	[ RRelsH | _ ] = RRels,			
-	{ _ , _ , RightNodePath , RChRels } = RRelsH,			%% Match the right child relation list
-	io:fwrite("my NodeID is: ~p~n", [ NodeId ]),	
-
-	MyRels = { 	NodeId , 
-			NodeRank , 
-			LeftNodePath ++ RightNodePath ++ [ NodeId ] , 
-			lists:umerge(lists:sort(LChRels),lists:sort(RChRels))           %% Merge the relation lists of immediate child nodes and
-		  },	
-											%% use this to create our relation list
-	[ MyRels ] ++  LRels ++ RRels 	
+			[ ] -> merge_subtrees1( [] ,
+						LsubTree,
+						RsubTree,
+						Self)
+	end
 .
 
-%%match_subtree ( { Op, L, R } , { Relname , Relalias } ) when ( Op == ">" or Op == "<" or Op == "=" or Op == "!=" )  ->
-%%	{ok}	
-%%.
+%% do join
+merge_subtrees1 (  [ ] , LsubTree , RsubTree, [ {type , NodeType } , {value , NodeVal} ] ) ->
+	case 
+		[ true || { leaf , true } <- LsubTree , { leaf , true } <- RsubTree ] of 
+		
+			[ true ] -> 
+					
+				[ LPred1 ] = [ LPr|| { predicate , LPr} <- LsubTree ] ,
+ 				[ RPred1 ] = [ RPr|| { predicate , RPr} <- RsubTree ] ,
+				[ {type, join} , 
+			     	{ joinpred , { [{type,NodeType},{value , NodeVal}] , LPred1 , RPred1 }} , 
+				{ left , LsubTree } , 
+				{ right, RsubTree } , { relation , null } ] ;
 
-%% colrefs and value expr are always leaf nodes
-%% 
 
-
+			[ ] -> [ {type, join} , { joinpred , NodeVal } , {left , LsubTree } , {right, RsubTree } , { relation , null } ]
+	end
+;
+%% do merge 
+merge_subtrees1 (  [ RelName ] , LsubTree , RsubTree, Self ) ->
+	%%io:fwrite("merge_subtrees1 with scan nodes called ~n~p~n~p", [ LsubTree , RsubTree ] ),
+	[ Lpred ] = [ Lpredicate || { predicate , Lpredicate } <- LsubTree ] , 
+	[ Rpred ] = [ Rpredicate || { predicate , Rpredicate } <- RsubTree ] , 
+	[ {type, scan} , { predicate , { Self , Lpred , Rpred } } , { relation , RelName } , { leaf , false} ]
+.
 
 %% @doc Produce list of scan nodes from parse tree	
 %%      including projections and selection predicates			
@@ -203,9 +133,9 @@ make_scan_nodes(ParseTree) ->
 %%	Brels = mk_brels(maps:get(from_clause, ParseTree)),
 %%	io:fwrite("Base relations: ~p", [ Brels ])
 %%	ScanNodes = get_sel(InitScanNodes)
-	ST = find_subtrees(ParseTree),
+	ST = find_subtrees1(ParseTree),
 	
-	io:fwrite("subtrees : ~p~n~n ========= ~w~n", [ ST , ST ])
+	io:fwrite("subtrees : ~p~n~n ========= ~n~n~w", [ ST , ST ])
 .
 
 %% @doc Initial planner. 
