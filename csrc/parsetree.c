ceae368229491fdcdf3d60509f5e69297a7f05f2
Charles Duffy <charles.duffy@gmail.com>
2015-12-11 19:17:20 +1100

    Constructor function for queryNode working Removed text parse tree pretty print functions

--- csrc/parsetree.c
+++ csrc/parsetree.c
@@ -6,17 +6,12 @@
 
 #define MAXBUFLEN 1024 //TODO get rid of this sort of thing
 
-#define new(nodetype) constr_##( ( nodetype * ) malloc ((size_t) sizeof(nodetype)))
+#define new(nodetype) new_##nodetype ( ( nodetype * ) malloc ((size_t) sizeof(nodetype)))
+
+/* constructor function definitiions */
+
+queryNode * new_queryNode ( queryNode *); 
 		      
-
-/* pretty printer fun decls. Farm these out to own files eventually */
-
-
-void prettyPrintSelectList(selectListNode *);
-void prettyPrintSelectNode(selectStmtNode *);
-void prettyPrintParseTree(queryNode *);
-void prettyPrintSexpr(scalarExpr *, int depth);
-
 /* TODO put these in a header file */
 
 selectListNode * get_select_list1 (selectStmtNode *);
@@ -66,7 +61,9 @@ might strip whitespace, find basic problems (corruption, too long, too short etc
 queryNode *parseQuery(char *queryText)
 {
   YY_BUFFER_STATE buf;
-  queryNode *qry = malloc(sizeof(queryNode));
+//  queryNode *qry = malloc(sizeof(queryNode));
+  queryNode *qry = new(queryNode);
+
   yyscan_t scanner;
 
   yylex_init(&scanner);
@@ -77,66 +74,4 @@ queryNode *parseQuery(char *queryText)
 //    prettyPrintParseTree(qry);
   return (qry);
 }
-
-void gv_Sexpr(scalarExpr * sExp, int depth)
-{
-
-  /* scalar expressions are tree-like structures. 
-     Basic recursive tree traversal algorithm here.
-   */
-  valueExprNode v;
-  char *oper[] = { "/", "*", "+", "-", "%", ">", "<", ">=", "<=", "OR", "AND", "NOT", "=", "!=" };      //****TODO fix this nonsense!
-
-  //print three dashes, then the value of this sexpr
-
-  printf("---");
-
-  switch (sExp->value.type) {
-
-    case UNDEFINED:
-      printf("[?:undefined]");
-      break;
-    case COLREF:
-      printf("[%s:colref]", sExp->value.value.column_val->colName);
-      break;
-    case TEXT:
-      printf("[%s:text]", sExp->value.value.text_val);
-      break;
-    case INT:
-      printf("[%d:integer]", sExp->value.value.integer_val);
-      break;
-    case NUM:
-      printf("[%f:numeric]", sExp->value.value.numeric_val);
-      break;
-    case OPER:
-      printf("[%s:operator]", *(oper + sExp->value.value.oper_val));
-      break;
-    case SEXPR:
-      printf("[SEXPR]");
-      break;
-  }
-
-  if (sExp->left != NULL) {
-    printf("\n");
-    drawbranch(depth + 1, ' ');
-    printf("|\n");
-    drawbranch(depth + 1, ' ');
-    printf("+");
-    prettyPrintSexpr(sExp->left, depth + 1);
-  }
-
-  if (sExp->right != NULL) {
-    printf("\n");
-    drawbranch(depth + 1, ' ');
-    printf("|\n");
-    drawbranch(depth + 1, ' ');
-    printf("\\");
-    prettyPrintSexpr(sExp->right, depth + 1);
-
-  }
-
-
-
-}
-
 
