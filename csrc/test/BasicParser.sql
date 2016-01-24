a6edc388bcf3d1d38fc0830ce7939801fa86664c
Charles Duffy <charles.duffy@gmail.com>
2016-01-24 17:44:12 +1100

    Add some tests

--- csrc/test/BasicParser.sql
+++ csrc/test/BasicParser.sql
@@ -3,7 +3,6 @@
 --https://trello.com/c/8YXSi9LP
 select a+1 from a, b where ( a + 1 ) > ( c + 1) / d;
 select foo , baz , (1+a) from bar , bat , boo where baz < 1;
-select a from foo as \"1\";
 --garbage strings 
 --
 --;
@@ -39,3 +38,6 @@ select a as c1 , b as c2 , c as c3 from A , B , C;
 select a/(2+4) as c1 , b+(6/(3-1)) as c2 , c as c3 , d*2 as c4 from A,B,C;
 --table expression aliases
 select tab1.a / (2 + 4) as c1 , tab1.b+(6/(3-1)) as c2 , tab2.c as c3 , tab3.d * 2  as c4 from A as tab1 ,B as tab2 ,C as tab3;
+select a from foo as \"1\";
+select \"1\".a from foo as \"1\";
+select tab1.a as col1, tab2.b as col2 from A as tab1, B as tab2 where col1 < 20 and col2  > 5 and col1 > col2 ;
